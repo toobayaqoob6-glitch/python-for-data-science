b5e33bcf1119c71beec96f03f9674843cc2b854e
Tooba yaqoob <toobayaqoob6@gmail.com>
2026-03-02 23:57:19 +0500

    operators.py

---
+++ operators.py
@@ -0,0 +1,32 @@
+#===BEGINNER LEVEL OPERATORS====#
+##==ARTHIMETIC OPERATORS==##
+a=int(input("enter your ist num : "))
+b=int(input("enter your 2nd num : "))
+print("ARTHIMETIC OPERATOR")
+print("addition (a+b):", a+b)
+print("subtraction(a-b):", a-b)
+print("division (a/b):", a/b)
+print("multiplication a*b :", a*b)
+print("modulus (a % b):", a%b)
+print("/n")
+
+##==COMPARISION OPERATORS==##
+print("COMPARISION OPERATORS")
+print("a == b:", a == b)
+print("a !=b:", a !=b)
+print("a>b:", a >b)
+print("a<b:", a <b)
+print("a >=b", a >=b)
+print("a<=b:",a<=b)
+print("/n")
+
+#===LOGICAL OPERATORS===#
+print("LOGICAL OPERATORS")
+condition1 = a > 0
+condition2= b< 0
+print("Both numbers positive:", condition1 and condition2)
+print("atleast one num positive:",condition1  or condition2)
+print("First number NOT positive:", not condition1)
+
+
+
