85046a63b3d0d1d0982c22aa4100fe8f0f567d72
Tooba yaqoob <toobayaqoob6@gmail.com>
2026-03-04 22:15:40 +0500

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,11 @@
+marks=int(input("enter your marks: "))
+if (marks>=90):
+    print ("GRADE A")
+elif(marks>=80):
+    print("GRADE B") 
+elif(marks>=70):
+    print("GRADE C")
+else:
+    print("GRADE D ")
+     
+    
