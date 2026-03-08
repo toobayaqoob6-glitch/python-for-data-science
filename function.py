4706e67f43dfb07d48d829d9da5af27ddb18e1c6
Tooba yaqoob <toobayaqoob6@gmail.com>
2026-03-08 19:37:18 +0500

    functions.py

---
+++ function.py
@@ -0,0 +1,26 @@
+# Simple Calculator using Functions
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+
+# User input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
